0c503bcd5ff6a465a6d460700592cd1b8c9b12d8
siddiquah anjum <50186478+siddiquah@users.noreply.github.com>
2022-04-19 00:21:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,32 +15,31 @@ function setClock() {
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`
     mn.style.transform = `rotateZ(${mm}deg)`
     sc.style.transform = `rotateZ(${ss}deg)`
+
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let am = h>=12 ? "PM" : "AM"
+
+    if (h>12) {
+        h = h - 12
+    }
+
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
 }
-
-let hours = document.getElementById('hour');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let ampm = document.getElementById('ampm');
-
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
-
-let am = h>=12 ? "PM" : "AM"
-
-if (h>12) {
-    h = h - 12
-}
-
-h = (h < 10) ? "0" + h : h
-m = (m < 10) ? "0" + m : m
-s = (s < 10) ? "0" + s : s
-
-
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
-
 setClock();
-
